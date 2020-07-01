22f2216c5679c5a7b7a9fe6c5fd8db414db01ac3
Евгений <jako.soll@gmail.com>
2020-07-01 16:38:03 +0300

    added file for refactored code. Added imp, cons, and main func

---
+++ dclnt_ref.py
@@ -0,0 +1,24 @@
+import ast
+import os
+import collections
+from typing import Union
+
+from nltk import pos_tag
+
+projects = [
+    'django',
+    'flask',
+    'pyramid',
+    'reddit',
+    'requests',
+    'sqlalchemy',
+]
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
+

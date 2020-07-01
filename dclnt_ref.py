496434bf7530f4b137e49f4f8c0ff03b79c1fbfe
Евгений <jako.soll@gmail.com>
2020-07-01 16:48:10 +0300

    added get_filenames() and parse_ast() and tested

--- dclnt_ref.py
+++ dclnt_ref.py
@@ -16,11 +16,37 @@ projects = [
 
 
 def get_filenames(path: str) -> list:
-    pass
+    file_names = []
+    for dirname, dirs, files in os.walk(path, topdown=True):
+        for file in files:
+            if file.endswith('.py'):
+                file_names.append(os.path.join(dirname, file))
+                if len(file_names) == 100:
+                    break
+    if file_names:
+        print('total %s files' % len(file_names))
+    return file_names
 
 
 def parse_ast(file_names: list, with_filenames=False, with_file_content=False):
-    pass
+    trees = []
+    for filename in file_names:
+        with open(filename, 'r', encoding='utf-8') as attempt_handler:
+            main_file_content = attempt_handler.read()
+        try:
+            tree = ast.parse(main_file_content)
+        except SyntaxError as e:
+            print(e)
+            tree = None
+        if with_filenames:
+            if with_file_content:
+                trees.append((filename, main_file_content, tree))
+            else:
+                trees.append((filename, tree))
+        else:
+            trees.append(tree)
+    print('trees generated')
+    return trees
 
 
 def get_node_name(trees):
